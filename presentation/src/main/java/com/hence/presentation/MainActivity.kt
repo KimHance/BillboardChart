ee3e830b79fcc83f2e4525b758806ad9b94e5c06
Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-01-03 15:30:21 +0900

    [chore]: app, data, domain, presentation모듈 분리

--- presentation/src/main/java/com/hence/presentation/MainActivity.kt
+++ presentation/src/main/java/com/hence/presentation/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.hence.billboardchart
+package com.hence.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
